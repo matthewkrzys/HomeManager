0de8e5bdf60d085c1f7814f5875bb9ba43d5367e
mateusz <matthewkrzys@gmail.com>
2025-02-17 20:55:03 +0100

    Fix Tocken

--- auth-service/src/main/java/com/home/manager/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/home/manager/auth_service/controller/AuthController.java
@@ -3,8 +3,10 @@ package com.home.manager.auth_service.controller;
 import com.home.manager.auth_service.model.UserEntity;
 import com.home.manager.auth_service.repository.UserRepository;
 import com.home.manager.auth_service.service.CustomUserDetailsService;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,10 +16,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @RestController
 @RequestMapping("/auth")
@@ -32,7 +39,7 @@ public class AuthController {
     @Autowired
     private CustomUserDetailsService userDetailsService;
 
-    private final String SECRET_KEY = "mysecretkey";
+    private final String SECRET_KEY = "3cfa76ef14937c1c0ea519f8fc057a80fcd04a7420f8e8bcd0a7567c272e007b";
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -46,17 +53,13 @@ public class AuthController {
     @PostMapping("/login")
     public Map<String, String> login(@RequestBody UserEntity user) {
         try {
-//            authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
-//            );
-
             UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUsername());
 
             String token = Jwts.builder()
                     .setSubject(userDetails.getUsername())
                     .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 godzina
-                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                    .setExpiration(new Date(new Date().getTime() + 1000 * 60)) // 1 godzina
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                     .compact();
 
             Map<String, String> response = new HashMap<>();
@@ -67,4 +70,36 @@ public class AuthController {
             throw new RuntimeException("Invalid username or password");
         }
     }
+
+    @GetMapping
+    public String checkToken(@RequestBody String token) {
+        return String.valueOf(isTokenExpired(token));
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
 }
